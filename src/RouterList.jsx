2bf2709ed41128ca472c084ec72319dca0042643
zeesee123 <zubinchadha@gmail.com>
2025-04-21 17:41:17 +0530

    new push

--- src/RouterList.jsx
+++ src/RouterList.jsx
@@ -7,10 +7,10 @@ import {
     Navigate,
     BrowserRouter,
 } from "react-router-dom";
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import Home from './screen/home/Home';
-import Support from './screen/Support/Support';
+import Header from './components/header/Header.jsx';
+import Footer from './components/footer/Footer.jsx';
+import Home from './screen/home/Home.jsx';
+import Support from './screen/Support/Support.jsx';
 function RouterList() {
     return (
         <>
